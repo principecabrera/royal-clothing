5cc609c4599eb827701c9435752f85513fa393ed
Principe Cabrera <pcabrera@Galactus.local>
2020-06-10 00:55:08 -0400

    Updated app with shop page, collection preview and collection item components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss'
 
-const MenuItem = ({title, image, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div 
         className="background-image"
-        style={{backgroundImage:`url(${image})`}}
+        style={{backgroundImage:`url(${imageUrl})`}}
         />
         <div className="content">
             <h1 className="title">{title}</h1>
@@ -15,4 +16,4 @@ const MenuItem = ({title, image, size}) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
